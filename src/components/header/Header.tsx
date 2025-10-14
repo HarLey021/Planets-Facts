343725c098a1372dc5fa04f5930374a6bb0b374b
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2025-10-14 14:41:40 +0400

    add mobile navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Navigation from "../navigation/Navigation";
 
 const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
   return (
     <>
-      <div className="w-full h-[70px] flex px-[24px] py-[16px] items-center justify-between">
-        <h1 className="text-[28px] text-white font-antonio tracking-[-1px]">
-          THE PLANETS
-        </h1>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="17"
-          viewBox="0 0 24 17"
-          fill="none"
-        >
-          <rect width="24" height="3" fill="white" />
-          <rect y="7" width="24" height="3" fill="white" />
-          <rect y="14" width="24" height="3" fill="white" />
-        </svg>
+      <div className="w-full h-[75px] mb-[20px]">
+        <div className=" flex px-[24px] py-[16px] items-center  justify-between">
+          <h1 className="text-[28px] text-white font-antonio tracking-[-1px]">
+            THE PLANETS
+          </h1>
+
+          <svg
+            className={`${menuOpen ? "opacity-25" : "opacity-100"}`}
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="17"
+            viewBox="0 0 24 17"
+            fill="none"
+          >
+            <rect width="24" height="3" fill="white" />
+            <rect y="7" width="24" height="3" fill="white" />
+            <rect y="14" width="24" height="3" fill="white" />
+          </svg>
+        </div>
+        <div>
+          <div className="w-full h-[1px] bg-white opacity-20 "></div>
+        </div>
       </div>
-      <div className="w-full h-[1px] bg-white opacity-20"></div>
+
+      {menuOpen && <Navigation />}
     </>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
     <>
       <div className="w-full min-h-screen bg-dark bg-[image:var(--bg-stars)]">
